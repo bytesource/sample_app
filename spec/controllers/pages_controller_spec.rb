e302c45dab75c0053e334e5a0db9235f795c86fd
Stefan Rohlfing <stefan.rohlfing@gmail.com>
2011-03-22 17:03:30 +0800

    Done with static pages.

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe PagesController do
+  render_views
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Sovonex International | Home")
+    end
+
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Sovonex International | Contact")
+    end
+  end
+
+  describe "Get 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Sovonex International | About")
+    end
+  end
+
+
+end
